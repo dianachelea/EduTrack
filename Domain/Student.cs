265f0eab314240faff37d21ea15866fa4fd0ef0c
Alexandra-Kerestely <alexandra-nicola.kerestely@student.tuiasi.ro>
2024-07-16 17:52:38 +0300

    started writing repository

---
+++ Domain/Student.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Domain
+{
+	public class Student
+	{
+		public int FirstName { get; set; }
+		public int LastName { get; set; }
+		public int	Email { get; set; }
+		public int Id{ get; set; }
+
+	}
+}
