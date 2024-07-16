0782e7a14f2c61f4951fdc346566c053fdd40798
Alexandra-Kerestely <alexandra-nicola.kerestely@student.tuiasi.ro>
2024-07-16 13:52:56 +0300

    created course domain objects

---
+++ Domain/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	public class Course 
+	{
+		public CourseDisplay CourseDetails { get; set; }
+		public string Description { get; set; }
+		public string ShortDescription { get; set; }
+
+		public string Category { get; set; }
+
+		public List<string> LearningTopics { get; set; }
+		public int StudentsEnrolled		{ get; set; }
+
+
+	}
+}
---
+++ Domain/CourseDisplay.cs
@@ -0,0 +1,21 @@
+﻿using Domain.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	public class CourseDisplay
+	{
+		public string Name { get; set; }
+		public string Prerequisites { get; set; }
+		
+		//enumul este definit in domain in enums
+		public CourseDifficulty Difficulty { get; set; }
+		
+		//FileContentResult este specific web mvc ului asa ca putem sa salvam doar partea de bytes in loc de tot obiectul
+		public Byte[] Image { get; set; }
+	}
+}
