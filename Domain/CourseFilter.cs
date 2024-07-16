8d8057f71c3205ea2b34c7e4bceca1a716c1adf0
Alexandra-Kerestely <alexandra-nicola.kerestely@student.tuiasi.ro>
2024-07-16 13:52:56 +0300

    created course domain objects

---
+++ Domain/CourseFilter.cs
@@ -0,0 +1,19 @@
+﻿using Domain.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	public class CourseFilter
+	{
+		public string Title { get; set; }
+		public List<string> Categories { get; set; }
+		public List<CourseDifficulty> Difficulties { get; set; }
+		public List<String> Prerequistes { get; set; }
+		public List<string> SortBy { get; set; }
+
+	}
+}
