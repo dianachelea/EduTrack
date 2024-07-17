d19266458749d453092efb124b0553d9ef4efb5c
Alexandra-Kerestely <alexandra-nicola.kerestely@student.tuiasi.ro>
2024-07-17 14:00:32 +0300

    finished writing course repository

--- Application/Interfaces/ICoursesRepository.cs
+++ Application/Interfaces/ICoursesRepository.cs
@@ -9,16 +9,16 @@ namespace Application.Interfaces
 {
 	 public interface ICoursesRepository
 	 {
-		Task<Course> GetCourse(string name);
+		Task<IEnumerable<Course>> GetCourse(string name);
 		Task<bool> AddCourse(Course course);
 		Task<bool> UpdateCourse(string name, Course course);
 		Task<bool> DeleteCourse(string name);
-		Task<List<CourseDisplay>> GetAllCourses();
-		Task<List<CourseDisplay>> GetCoursesByFilter(CourseFilter filter);
-		Task<List<Course>> GetCoursesByStudentEmail(string studentEmail);
-		Task<Student> GetAllStudentsEnrolled(string name);
+		Task<IEnumerable<CourseDisplay>> GetAllCourses();
+		Task<IEnumerable<CourseDisplay>> GetCoursesByFilter(CourseFilter filter);
+		Task<IEnumerable<Course>> GetCoursesByStudentEmail(string studentEmail);
+		Task<IEnumerable<Student>> GetAllStudentsEnrolled(string name);
 		//putem folosi o val hardcoded pt numar pt ca ar fi la fel pe toate paginile
-		Task<List<CourseDisplay>> GetRelatedCourses(string name);
+		Task<IEnumerable<CourseDisplay>> GetRelatedCourses(string name);
 
 
 
--- Domain/CourseFilter.cs
+++ Domain/CourseFilter.cs
@@ -12,8 +12,8 @@ namespace Domain
 		public string Title { get; set; }
 		public List<string> Categories { get; set; }
 		public List<int> Difficulties { get; set; }
-		public List<String> Prerequistes { get; set; }
-		public List<string> SortBy { get; set; }
+		public List<string> Prerequistes { get; set; }
+		public string SortBy { get; set; }
 
 	}
 }
