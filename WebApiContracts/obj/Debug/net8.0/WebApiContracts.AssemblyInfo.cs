46354caf756ac5793d6c4df793d27498611def93
Andrei <andrei.zacordonet@student.tuiasi.ro>
2024-07-21 14:44:55 +0300

    "to be ignored"

--- WebApiContracts/obj/Debug/net8.0/WebApiContracts.AssemblyInfo.cs
+++ WebApiContracts/obj/Debug/net8.0/WebApiContracts.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cc7fc0e3f83b62be210c1ec70463b121c1f1c6")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f300ae98ed362cc4f9c9cbac9f9a391b1ffc29d5")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
