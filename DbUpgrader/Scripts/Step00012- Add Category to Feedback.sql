b3160ccf2ee4deb6544f11b87ef04130048f2d8b
Andrei <andrei.zacordonet@student.tuiasi.ro>
2024-07-22 13:14:56 +0300

    small changes

--- DbUpgrader/Scripts/Step00012- Add Category to Feedback.sql
+++ DbUpgrader/Scripts/Step00012- Add Category to Feedback.sql
@@ -3,17 +3,3 @@ BEGIN
     ALTER TABLE [SummerPractice].[Feedback]
     ADD [Feedback_category] nvarchar(50);
 END;
-
---this should be included in a different script in order to run properly
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE WHERE TABLE_NAME = 'Feedback' AND CONSTRAINT_NAME = 'CHK_Feedback_Category' AND TABLE_SCHEMA = 'SummerPractice')
-BEGIN
-    ALTER TABLE [SummerPractice].[Feedback]
-    ADD CONSTRAINT CHK_Feedback_Category CHECK ([Feedback_category] IN ('Content Quality', 'User Experience', 'Technical Performance', 'Educational Tools', 'Assessment and Feedback'));
-END;
-
---this should be included in a different script in order to run properly
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Feedback' AND COLUMN_NAME = 'Feedback_date' AND TABLE_SCHEMA = 'SummerPractice')
-BEGIN
-    ALTER TABLE [SummerPractice].[Feedback]
-    ADD [Feedback_date] DATETIME DEFAULT GETDATE();
-END;
